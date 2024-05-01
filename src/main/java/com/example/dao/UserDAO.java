2710439efa5633dc433d1775611aa7dbcf87fc38
AyodhyaKarunanayake <ayodhyakarunanayake@gmail.com>
2024-05-01 20:20:20 +0530

    user signup

--- src/main/java/com/example/dao/UserDAO.java
+++ src/main/java/com/example/dao/UserDAO.java
@@ -1,6 +1,7 @@
 package com.example.dao;
 
 import com.example.DatabaseConnection;
+import com.example.entity.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,6 +13,7 @@ public class UserDAO {
 
     // SQL queries
     private static final String AUTHENTICATE_USER_QUERY = "SELECT * FROM user WHERE userName = ? AND password = ?";
+    private static final String INSERT_USER_QUERY = "INSERT INTO user (email, userName, password, role) VALUES (?, ?, ?, ?)";
     private static Connection connection;
 
     public UserDAO() {
@@ -27,6 +29,22 @@ public class UserDAO {
             try (ResultSet resultSet = statement.executeQuery()) {
                 return resultSet.next(); // Return true if the result set has at least one row
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean addUser(User user) {
+        try {
+            PreparedStatement statement = connection.prepareStatement(INSERT_USER_QUERY);
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getUserName());
+            statement.setString(3, user.getPassword());
+            statement.setString(4, user.getRole());
+
+            int rowsInserted = statement.executeUpdate();
+            return rowsInserted > 0;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
